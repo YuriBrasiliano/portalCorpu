513f1f089d503220c076e9110ebc79f24c15bfe5
Yuri Brasiliano <yuribrasiliano37@gmail.com>
2023-09-23 01:50:03 -0300

    start second page

--- src/components/specialties/banner/SpecialtiesBanner.jsx
+++ src/components/specialties/banner/SpecialtiesBanner.jsx
@@ -1,10 +1,26 @@
 import "./specialties.scss"
+import bannerimg from "../../../assets/images/c-1.webp"
 import AnimatedComponent from '../../../utils/AnimatedComponent';
 
 export default function SpecialtiesBanner(){
     return(
         <AnimatedComponent duration={1.4} delay={1}>
-            <h1>APAREÇA DE sSESSAMsO </h1>
+            <div className="banner-spec-container">
+            <div className="text-banner-spec">
+              <p className="title">Especialidades da<br />
+              <span className='subtitle'> GCÓR</span></p>
+              <p className="paragraph">
+              Temos várias especialidades médicas para atender todas as suas necessidades
+              </p>
+
+            </div>
+            <div className="image-banner-spec">
+              <img src={bannerimg} alt="Nossa Especialidades" />
+            </div>
+          </div>
+
+
+
         </AnimatedComponent>
     )
 }
